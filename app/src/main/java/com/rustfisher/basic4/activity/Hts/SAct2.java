5f5f16e4993f27865a68e41406ccabdc9af1c934
RustFisher <rustfisher@foxmail.com>
2018-06-19 11:59:02 +0800

    获取view的宽高信息

--- app/src/main/java/com/rustfisher/basic4/activity/Hts/SAct2.java
+++ app/src/main/java/com/rustfisher/basic4/activity/Hts/SAct2.java
@@ -3,7 +3,9 @@ package com.rustfisher.basic4.activity.Hts;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import com.rustfisher.basic4.R;
 import com.rustfisher.basic4.activity.SAct;
@@ -11,17 +13,27 @@ import com.rustfisher.basic4.service.HowToStopService;
 
 public class SAct2 extends SAct {
 
+    Button mBtn1;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         mLogPre = "[act2] ";
         super.onCreate(savedInstanceState);
-        findViewById(R.id.btn1).setOnClickListener(new View.OnClickListener() {
+        mBtn1 = findViewById(R.id.btn1);
+        mBtn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), SAct3.class));
             }
         });
         bindService(new Intent(getApplicationContext(), HowToStopService.class), mConn, BIND_AUTO_CREATE);
+        Log.d(TAG, "mBtn1 post runnable");
+        mBtn1.post(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "mBtn1: " + mBtn1.getWidth() + ", " + mBtn1.getHeight());
+            }
+        });
     }
 
     @Override
